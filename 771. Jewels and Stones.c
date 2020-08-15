15d035f584961b2f6cf3537f6567b2d56e9202ae
LZhenHong <levine5blue@gmail.com>
2020-08-15 15:37:17 +0800

    [ADD] Jewels and Stones.

---
+++ 771. Jewels and Stones.c
@@ -0,0 +1,11 @@
+int numJewelsInStones(char* J, char* S) {
+	int nums = 0;
+	for (int i = 0; i < strlen(J); ++i) {
+		for (int j = 0; j < strlen(S); ++j) {
+			if (J[i] == S[j]) {
+				++count;
+			}
+		}
+	}
+	return count;
+}
